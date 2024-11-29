a4a2ecd68fe2447865637226ae1fa51e6de25e40
Alex Ford <alex96ford19@gmail.com>
2024-11-29 16:53:37 +0000

    Make a start adding the spirit component

---
+++ src/components/Spirit.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+import { defineComponent, ref, watch, onMounted } from "vue";
+
+export default defineComponent({
+  name: "Spirit",
+  props: {
+    spirit: {
+      type: Object,
+      required: true,
+    },
+  },
+})
+</script>
+
+
+<template>
+  <div class="whisky-container">
+    <div class="row">
+      <div class="col-8">
+        <h2 class="whisky-name">{{ spirit.name }}</h2>
+        <p class="whisky-description">{{ spirit.description }}</p>
+        <div class="whisky-details">
+          <p class="whisky-location">{{ spirit.country }} - {{ spirit.region }}</p>
+
+          <br />
+          <br /><br />
+
+          <p class="whisky-info"><b>Type:</b> {{ spirit.type }}<br /></p>
+          <p class="whisky-info"><b>Price:</b> £{{ spirit.price }}.00<br /></p>
+          <p class="whisky-info"><b>Flavour:</b> {{ spirit.flavour }}<br /></p>
+          <p class="whisky-info"><b>Brand:</b> {{ spirit.brand }}<br /></p>
+          <p class="whisky-info"><b>Cask:</b> {{ spirit.cask }}<br /></p>
+        </div>
+      </div>
+
+      <div class="col-4">
+        <img :src="spirit.image" :alt="spirit.name" class="whisky-image" />
+      </div>
+    </div>
+
+    <p class="whisky-review">
+      <strong>Review:</strong> {{ spirit.review }}
+    </p>
+
+    <a :href="spirit.link" target="_blank" class="button">Info</a>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,33 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Spirit from '../components/Spirit.vue'
+import { ref, onMounted } from 'vue';
+
+const spirits = ref([])
+
+onMounted(async () => {
+  try {
+    const response = await fetch(
+      `https://api.allorigins.win/raw?url=${encodeURIComponent(
+        'https://api.fordsdevelopment.co.uk/spirits'
+      )}`
+    )
+    console.log(response)
+    spirits.value = await response.json()
+  } catch (error) {
+    console.error('Error fetching spirits:', error)
+  }
+})
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div v-if="spirits.length > 0">
+      <Spirit
+        v-for="(spirit, index) in spirits"
+        :key="index"
+        :spirit="spirit"
+      />
+    </div>
+    <p v-else>Loading spirits...</p>
   </main>
 </template>
