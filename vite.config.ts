8f2bbfc6f6531d332a03c49de6aa254f5255beaa
Alex Ford <alex96ford19@gmail.com>
2024-11-29 16:57:35 +0000

    Update base path

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/Vue-Template/',
+  base: '/Drinks/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
