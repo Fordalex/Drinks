144e41600a84f16d626cc776c37e10209da386f7
Alex Ford <alex96ford19@gmail.com>
2024-12-07 12:30:12 +0000

    Use save navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -21,7 +21,7 @@ export default defineComponent({
   <v-app>
     <!-- App Bar -->
     <v-app-bar app>
-      <v-toolbar-title>Drinks - {{ user.given_name }}</v-toolbar-title>
+      <v-toolbar-title>Drinks - {{ user?.given_name }}</v-toolbar-title>
       <v-spacer></v-spacer>
       <!-- Desktop Links (hidden on mobile) -->
       <div class="d-none d-md-flex">
