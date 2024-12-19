9f837f704d6d55e0962294914103982582f5e418
Alex Ford <alex96ford19@gmail.com>
2024-12-19 19:35:52 +0000

    Clean up company cards

--- src/components/Company.vue
+++ src/components/Company.vue
@@ -14,8 +14,13 @@ export default defineComponent({
       return `/Drinks/#/companies/${props.company.id}`
     })
 
+    const brands = computed(() => {
+      return props.company.brands.map((brand: any) => brand.name).join(', ')
+    })
+
     return {
       companyLink,
+      brands,
     }
   }
 })
@@ -36,36 +41,11 @@ export default defineComponent({
 
     <v-card-item>
       <v-card-title>{{ company.name }}</v-card-title>
-
-      <v-card-subtitle>
-        <span class="me-1">Local Favorite</span>
-
-        <v-icon color="error" icon="mdi-fire-circle" size="small"></v-icon>
-      </v-card-subtitle>
     </v-card-item>
 
     <v-card-item>
-      <p>Brands: {{  company.brands }}</p>
+      <p>Brands: {{ brands }}</p>
     </v-card-item>
-
-    <v-card-text>
-      <v-row align="center" class="mx-0">
-        <v-rating
-          :model-value="4.5"
-          color="amber"
-          density="compact"
-          size="small"
-          half-increments
-          readonly
-        ></v-rating>
-
-        <div class="text-grey ms-4">4.5 (413)</div>
-      </v-row>
-
-      <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
-
-      <div>{{ company.description || 'No description.' }}</div>
-    </v-card-text>
 
     <v-card-actions>
       <v-btn :href="companyLink" color="deep-purple-lighten-2" text="More" block border></v-btn>
